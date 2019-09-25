cd2ccaca8ffcc27dd585675f3abecbfc43c35794
Panpaporn <panpapornd@gmail.com>
2019-09-25 18:11:01 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,41 @@ import WordCard from './WordCard';
 import './App.css';
 const word = "Hello";
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+    
+          play: false,
+          pause: true
+    
+        };
+    
+        this.url = "http://streaming11.tdiradio.com:8000/tdiradiokragujevac.mp3";
+        this.audio = new Audio(this.url);
+    
+      }
+
+      play = () =>{
+        this.setState({
+          play: true,
+          pause: false
+        });
+        console.log(this.audio);
+        this.audio.play();
+      }
+    
+      pause =() =>{
+      this.setState({ play: false, pause: true });
+        this.audio.pause();
+      }
+
     render() {
         return ( 
             <div>{ <WordCard value="PACK"/>}
-               
-               
-        
-            </div>
+            <button onClick={this.play}>Play</button>
+            <button onClick={this.pause}>Pause</button>
+           </div>
+
         );
     }
 }
